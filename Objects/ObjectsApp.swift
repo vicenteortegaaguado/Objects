48d26b85cef6986d4d928f86740e075bb3325a3c
Vicente Ortega Aguado <vicenteortegaaguadodevelopment@gmail.com>
2024-05-08 05:54:23 +0200

    Initial Commit

---
+++ Objects/ObjectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectsApp.swift
+//  Objects
+//
+//  Created by Vicente Ortega Aguado on 8/5/24.
+//
+
+import SwiftUI
+
+@main
+struct ObjectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
